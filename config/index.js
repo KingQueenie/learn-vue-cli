873e45f1edc83a143fd3c68bdb779a0c941e4d0a
马金坤 <majinkun>
2019-12-06 15:21:31 +0800

    测试

---
+++ config/index.js
@@ -0,0 +1,5 @@
+//  github api 基础地址
+exports.baseURL = 'https://api.github.com'
+// github 组织名称
+exports.orgName = 'learn-queen'
+exports.token = '2 e 2 2 8 0 f 7 0 5 6 4 6 8 b 7 9 9 e 9 4 c 4 d 9 0 c d 6 7 2 d 3 3 4 3 3 c 4 d'
